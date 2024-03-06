2012756c35025a21f572480f9078bc501a25f732
Gabriel Nazate <christian.nazate@epn.edu.ec>
2024-03-06 16:05:44 -0500

    Fix: Reasigned global var to local var

--- scripts/cereza_game/ball.gd
+++ scripts/cereza_game/ball.gd
@@ -3,14 +3,16 @@ extends Area2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-@export var speed = 100
+@export var ball_speed = 150
+var bottom_limit = 139
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 #var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var velocity = Vector2(0,0)
 
 func _physics_process(delta):
-	global_position.y += global_var.ball_speed*delta 
-	if global_position.y > global_var.bottom_limit:
+	global_position.y += ball_speed*delta 
+	if global_position.y > bottom_limit:
 		die()
 
 func _on_body_entered(body):
